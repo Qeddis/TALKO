3780d4bf455004b0d23fa623c5e91696c7aa50e4
Qeddis <madara.inter@gmail.com>
2026-06-24 14:05:30 +0330

    Add user registration

--- database/db.py
+++ database/db.py
@@ -1,30 +1,48 @@
 from pathlib import Path
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import select
 from database.models import Base, User
-from sqlalchemy import select, update
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}/data/bot.db"
-
-engine = create_async_engine(DATABASE_URL)
-SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 (BASE_DIR / "data").mkdir(exist_ok=True)
 
 DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}/data/bot.db"
 
+engine = create_async_engine(DATABASE_URL)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
-async def get_waiting_user():
-    ...
-
-async def set_partner(user_id, partner_id):
-    ...
-
-async def end_chat(user_id):
-    ...
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_user(telegram_id: int):
+    async with SessionLocal() as session:
+        result = await session.execute(
+            select(User).where(User.telegram_id == telegram_id)
+        )
+        return result.scalar_one_or_none()
+
+
+async def add_user(telegram_id: int, username: str | None):
+    async with SessionLocal() as session:
+        result = await session.execute(
+            select(User).where(User.telegram_id == telegram_id)
+        )
+
+        user = result.scalar_one_or_none()
+
+        if user:
+            return user
+
+        user = User(
+            telegram_id=telegram_id,
+            username=username,
+        )
+
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+        return user
--- handlers/start.py
+++ handlers/start.py
@@ -3,11 +3,18 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from keyboards.menu import main_menu
+from database.db import add_user
 
 router = Router()
 
+
 @router.message(CommandStart())
 async def start(message: Message):
+
+    await add_user(
+        telegram_id=message.from_user.id,
+        username=message.from_user.username,
+    )
 
     await message.answer(
         "👋 به ربات چت ناشناس خوش آمدی",
